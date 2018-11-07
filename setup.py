fd4e09c2f442109c67ed45698a8509a72a7744d4
Adam R. Rivers <adam.rivers@ars.usda.gov>
2018-11-07 09:44:49 -0500

    format modifications to setup.pu

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+"""setup.py: python package setup for ITSxpress
+
+"""
+
 from setuptools import setup
 
 setup(
     name='itsxpress',
-    version='1.7.0',
+    version='1.7.1',
     packages=['itsxpress'],
     license='License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
     description="Rapidly trim sequences down to their Internally Transcribed Spacer (ITS) regions",
@@ -13,12 +17,12 @@ setup(
                  'Development Status :: 3 - Alpha'],
     keywords='Amplicon sequencing fungal ITS',
     url='http://github.com/usda-ars-gbru/itsxpress',
-    test_suite ='nose.collector',
+    test_suite='nose.collector',
     author='Adam R. Rivers',
     author_email='adam.rivers@ars.usda.gov',
     install_requires=['biopython>=1.60'],
     python_requires='>3.5',
     tests_require=['nose'],
     include_package_data=True,
-    entry_points= {'console_scripts':['itsxpress=itsxpress.main:main']},
+    entry_points={'console_scripts':['itsxpress=itsxpress.main:main']},
     zip_safe=False)
