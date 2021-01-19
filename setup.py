3adcaf0d2a5e5d791a91f45c3bd66cb8da1bd9a4
MeghaNagarmunoli <meghanagarmunoli@gmail.com>
2021-01-19 13:40:35 -0500

    changed dependency install path to etc

--- setup.py
+++ setup.py
@@ -8,17 +8,11 @@ import os
 
 def custom_command():
     import sys
-    cmd  = 'wget https://sourceforge.net/projects/bbmap/files/BBMap_38.60.tar.gz -O /tmp/BBMap_38.60.tar.gz; tar -xvf /tmp/BBMap_38.60.tar.gz -C /tmp; echo magu;export PATH=$PATH:/tmp/bbmap; echo $PATH'
+    cmd  = 'wget https://sourceforge.net/projects/bbmap/files/BBMap_38.60.tar.gz -O /etc/BBMap_38.60.tar.gz; tar -xvf /etc/BBMap_38.60.tar.gz -C /etc'
 
-    cmd1 = 'wget https://github.com/torognes/vsearch/releases/download/v2.13.6/vsearch-2.13.6-linux-x86_64.tar.gz -O /tmp/vsearch-2.13.6-linux-x86_64.tar.gz; tar -xvf /tmp/vsearch-2.13.6-linux-x86_64.tar.gz -C /tmp'
-        # - wget https://github.com/torognes/vsearch/releases/download/v2.13.6/vsearch-2.13.6-linux-x86_64.tar.gz -O /tmp/vsearch-2.13.6-linux-x86_64.tar.gz
-        # - tar -xvf /tmp/vsearch-2.13.6-linux-x86_64.tar.gz
-        # - export PATH=$PATH:$PWDtar -xvf /tmp/vsearch-2.13.6-linux-x86_64.tar.gz
-        # - wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz -O /tmp/hmmer-3.1b2-linux-intel-x86_64.tar.gz
-        # - tar -xvf /tmp/hmmer-3.1b2-linux-intel-x86_64.tar.gz
-        # - ls -la
-        # - export PATH=$PATH:$PWD/hmmer-3.1b2-linux-intel-x86_64/binaries
-    cmd2 = 'wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz -O /tmp/hmmer-3.1b2-linux-intel-x86_64.tar.gz; tar -xvf /tmp/hmmer-3.1b2-linux-intel-x86_64.tar.gz -C /tmp'
+    cmd1 = 'wget https://github.com/torognes/vsearch/releases/download/v2.13.6/vsearch-2.13.6-linux-x86_64.tar.gz -O /etc/vsearch-2.13.6-linux-x86_64.tar.gz; tar -xvf /etc/vsearch-2.13.6-linux-x86_64.tar.gz -C /etc'
+
+    cmd2 = 'wget http://eddylab.org/software/hmmer3/3.1b2/hmmer-3.1b2-linux-intel-x86_64.tar.gz -O /etc/hmmer-3.1b2-linux-intel-x86_64.tar.gz; tar -xvf /etc/hmmer-3.1b2-linux-intel-x86_64.tar.gz -C /etc'
 
     if sys.platform in ['darwin', 'linux']:
         os.system(cmd)
@@ -30,7 +24,6 @@ class CustomInstallCommand(install):
     def run(self):
         install.run(self)
         print("custom install")     
-        # raise AttributeError 
         custom_command()
 
 # def _post_install(setup):
