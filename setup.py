a264e497e2cb38c1f7751f33d983a474239b6d97
seina001 <seinarsson@ufl.edu>
2023-01-24 05:35:58 -0500

    Update conda depenencies

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@
 """
 
 from setuptools import setup
+import subprocess
+
 if __name__ == "__main__":
+    subprocess.run("conda install -y -c conda-forge hmmer",shell=True)
+    subprocess.run("conda install -y -c bioconda vsearch",shell=True)
     setup()
 
