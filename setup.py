116ceb1bc663e188a888fa493a987bd0e6336fec
seina001 <seinarsson@ufl.edu>
2023-01-24 14:00:17 -0500

    Testing change to setup

--- setup.py
+++ setup.py
@@ -11,11 +11,15 @@ if __name__ == "__main__":
     #Check dependency versions and assert
 
 
-    
+
     outp_hmmer = str(subprocess.run("conda list hmmer",shell=True,stdout=subprocess.PIPE))
     outp_bbmap = str(subprocess.run("conda list bbmap",shell=True,stdout=subprocess.PIPE))
     outp_vsearch = str(subprocess.run("conda list vsearch",shell=True,stdout=subprocess.PIPE))
     assert (outp_hmmer.find('3.1b2') != -1),"hmmer doesn't seem to be version 3.1b2, create a fresh conda environment and install itsxpress again"
     assert (outp_bbmap.find('38.69') != -1),"bbmap doesn't seem to be version 38.69, create a fresh conda environment and install itsxpress again"
     assert (outp_vsearch.find('2.21.1') != -1),"vsearch doesn't seem to be version 2.21.1, create a fresh conda environment and install itsxpress"
+    print(outp_hmmer)
+    print(outp_bbmap)
+    print(outp_vsearch)
+    
     setup()
