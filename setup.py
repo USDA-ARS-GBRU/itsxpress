5cec268eaeab2220f8e232a2143e88e39c4f0b17
seina001 <seinarsson@ufl.edu>
2023-02-13 12:29:19 -0500

    Adding q2 plugin

--- setup.py
+++ setup.py
@@ -6,7 +6,5 @@ from setuptools import setup
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.run("conda install -y -c conda-forge hmmer",shell=True)
-    subprocess.run("conda install -y -c bioconda vsearch",shell=True)
     setup()
 
