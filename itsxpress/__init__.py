0ff276bb435743cc514ea93700bc8aea3e11506f
seina001 <seinarsson@ufl.edu>
2023-02-14 11:45:03 -0500

    Adding q2 plugin

--- itsxpress/__init__.py
+++ itsxpress/__init__.py
@@ -6,7 +6,12 @@ import itsxpress.SeqSample
 import itsxpress.SeqSampleNotPaired
 import itsxpress.SeqSamplePaired
 
-import itsxpress.q2_itsxpress
-import itsxpress.plugin_setup
+try:
+    import itsxpress.q2_itsxpress
+    import itsxpress.plugin_setup
+except Exception as e:
+    print("could not initialize the Qiime plugin portion of ITSxpress. Command line ITSxpress should work normally")
+    print(e)
+    pass
 
 __all__ = ["main", "definitions"]
